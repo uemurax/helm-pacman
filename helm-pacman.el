f7618059642096b01438f1a8b97a163f79bb344a
UEMURA Taichi <t.uemura00@gmail.com>
2016-03-19 00:21:06 +0900

    helm for pacman query

--- helm-pacman.el
+++ helm-pacman.el
@@ -4,43 +4,59 @@
 ;;
 ;; Author: Taichi Uemura <t.uemura00@gmail.com>
 ;; License: GPL3
-;; Time-stamp: <2016-03-18 23:45:04 tuemura>
+;; Time-stamp: <2016-03-19 00:17:27 tuemura>
 ;;
 ;;; Code:
 
 (require 'helm)
 
 (eval-when-compile
-  (defmacro make-helm-action (action function)
-    (let ((action-doc (format "Run `%s' in `helm-pacman' session." function)))
+  (defmacro make-helm-action (action function &optional also-persistent)
+    (let ((action-doc (format "Run `%s' in `helm-pacman' session." function))
+          (persistent (intern (format "%s-persistent" action)))
+          (persistent-doc (format "Run `%s' in `helm-pacman' session without exitting session."
+                                  function)))
       `(progn
          (defun ,action ()
+           ,action-doc
            (interactive)
            (with-helm-alive-p
              (helm-exit-and-execute-action ',function)))
-         (put ',action 'helm-only t)))))
+         (put ',action 'helm-only t)
+         ,@(when also-persistent
+             `((defun ,persistent ()
+                 ,persistent-doc
+                 (interactive)
+                 (with-helm-alive-p
+                   (helm-attrset 'pacman-persistent-action '(,function . never-split))
+                   (helm-execute-persistent-action 'pacman-persistent-action)))
+               (put ',persistent 'helm-only t)))))))
+
+(defun helm-pacman-make-arguments ()
+  "Make space-separated arguments from `helm-marked-candidates'."
+  (mapconcat 'identity (helm-marked-candidates) " "))
+
+(defmacro helm-pacman-defaction (name operation &optional sudo)
+  (let ((command (format "%spacman -%s "
+                         (if sudo "sudo " "")
+                         operation)))
+    `(progn
+       (defun ,name (&rest _ignore)
+         (async-shell-command (concat ,command
+                                      (helm-pacman-make-arguments)))))))
+
+;;;; Sync
 
 (defun helm-pacman-sync ()
   (split-string (shell-command-to-string "pacman -Slq")
                 "\n"))
 
-(defun helm-pacman-sync-info (_ignore)
-  (switch-to-buffer "*helm-pacman-sync-info*")
-  (erase-buffer)
-  (shell-command (concat "pacman -Si "
-                         (mapconcat 'identity (helm-marked-candidates) " "))
-                 t))
+(helm-pacman-defaction helm-pacman-sync-info "Si")
 
-(defun helm-pacman-sync-install (_ignore)
-  (async-shell-command (concat "sudo pacman -S "
-                         (mapconcat 'identity (helm-marked-candidates) " "))))
-
+(helm-pacman-defaction helm-pacman-sync-install "S" t)
 (make-helm-action helm-pacman-sync-run-install helm-pacman-sync-install)
 
-(defun helm-pacman-sync-download (_ignore)
-  (async-shell-command (concat "sudo pacman -Sw "
-                               (mapconcat 'identity (helm-marked-candidates) " "))))
-
+(helm-pacman-defaction helm-pacman-sync-download "Sw" t)
 (make-helm-action helm-pacman-sync-run-download helm-pacman-sync-download)
 
 (defvar helm-pacman-sync-actions
@@ -63,11 +79,44 @@
     :action 'helm-pacman-sync-actions
     :keymap helm-pacman-sync-keymap))
 
+;;;; Query
+
+(defun helm-pacman-query ()
+  (split-string (shell-command-to-string "pacman -Qq")
+                "\n"))
+
+(helm-pacman-defaction helm-pacman-query-info "Qi")
+
+(helm-pacman-defaction helm-pacman-query-list "Ql")
+(make-helm-action helm-pacman-query-run-list helm-pacman-query-list t)
+
+(defvar helm-pacman-query-actions
+  (helm-make-actions
+   "Show package(s)" 'helm-pacman-query-info
+   "List package(s)' files" 'helm-pacman-query-list))
+
+(defvar helm-pacman-query-keymap
+  (let ((m (make-sparse-keymap)))
+    (set-keymap-parent m helm-map)
+    (dolist (v '(("M-L" . helm-pacman-query-run-list)
+                 ("C-c l" . helm-pacman-query-run-list-persistent)))
+      (define-key m (kbd (car v)) (cdr v)))
+    m))
+
+(defun helm-pacman-build-query-source (name &rest args)
+  (helm-build-sync-source name
+    :candidates (helm-pacman-query)
+    :action 'helm-pacman-query-actions
+    :keymap helm-pacman-query-keymap))
+
+;;;; Put together
+
 ;;;###autoload
 (defun helm-pacman ()
   "Helm for Pacman."
   (interactive)
-  (helm :sources (list (helm-pacman-build-sync-source "Sync"))
+  (helm :sources (list (helm-pacman-build-sync-source "Sync")
+                       (helm-pacman-build-query-source "Query"))
         :buffer "*helm-pacman*"))
 
 (provide 'helm-pacman)
